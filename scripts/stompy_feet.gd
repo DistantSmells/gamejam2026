959eed899f071a113bb3cf1bd89800778b7a0893
DistantSmells <144760696+DistantSmells@users.noreply.github.com>
2026-05-10 01:13:37 +1000

    Major Addition
    - Falling Pencils
    - Stomping Feet
    - Crank & Platform
    - Books

---
+++ scripts/stompy_feet.gd
@@ -0,0 +1,11 @@
+extends Node2D
+# Animation
+
+@export var frequency : float = 1.0
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "StompL":
+		$AnimationPlayer.play("StompR")
+	if anim_name == "StompR":
+		$AnimationPlayer.play("StompL")
