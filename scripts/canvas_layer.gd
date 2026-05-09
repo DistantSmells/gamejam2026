b7bd2b465332884cd2ea54b0e926d186727df234
Bhavnoor Singh <KidNamedFinguh@protonmail.com>
2026-05-09 10:32:36 +1000

    updated from main

--- scripts/canvas_layer.gd
+++
@@ -1,8 +0,0 @@
-extends CanvasLayer
-
-func _on_start_button_pressed() -> void:
-	# Replace with the actual path to your guinea_pig scene
-	get_tree().change_scene_to_file("res://scenes/guinea_pig.tscn")
-
-func _on_quit_button_pressed() -> void:
-	get_tree().quit()
--- scripts/pickable.gd
+++ scripts/pickable.gd
@@ -1,15 +1,44 @@
 extends Area2D
 
-@onready var parent : Node2D = get_parent()
-@export_subgroup("Grab Type")
-enum GrabType {UPDATE_PARENT_POSITION, NONE}
-@export var grab_type: GrabType
+@export_subgroup("Settings")
+@export var parent : Node2D
+## Changes if the parent position get's updated when pickable is moved.
+#@export var grab_type: GrabType
+#enum GrabType {UPDATE_PARENT_POSITION, NONE}
 
+# Private variables
+var hovered : bool = false
+var one_shot : bool = false
+#var is_held : bool = false
+# Use callback from signals for information
+signal grabbed
+signal dropped
 
+func _process(delta: float) -> void:
+	# Determine if grabbed or dropped -> Sets whether held...
+	# If mouse pressed while hovering "grab"
+	if Input.is_action_just_pressed("LMB") and hovered == true:
+		emit_signal("grabbed")
+		print("Grabbed")
+		one_shot = true
+	# If mouse is released while hovering drop
+	if Input.is_action_just_released("LMB") and one_shot:
+		emit_signal("dropped")
+		one_shot = false
+		print("Dropped")
+	
+	## Actual processing parent stuff...
+	#if is_held:
+		#if grab_type == GrabType.UPDATE_PARENT_POSITION:
+			#pass
 
-func _on_area_entered(area: Area2D) -> void:
-	pass # Replace with function body.
+func _ready() -> void:
+	#If not parent is set, get parent
+	if parent == null:
+		parent = get_parent()
 
+func _on_mouse_entered() -> void:
+	hovered = true
 
-func _on_area_exited(area: Area2D) -> void:
-	pass # Replace with function body.
+func _on_mouse_exited() -> void:
+	hovered = false
