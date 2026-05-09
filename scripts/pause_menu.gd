1a0c1478117e2a92acaee1b520acf380baca8dcf
Bhavnoor Singh <KidNamedFinguh@protonmail.com>
2026-05-09 10:45:17 +1000

    the actual working pause menu

---
+++ scripts/pause_menu.gd
@@ -0,0 +1,19 @@
+extends CanvasLayer
+
+func _ready() -> void:
+	hide() # Start hidden
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"): # Usually the "Esc" key
+		toggle_pause()
+
+func toggle_pause() -> void:
+	var new_pause_state = !get_tree().paused
+	get_tree().paused = new_pause_state
+	visible = new_pause_state
+
+func _on_resume_button_pressed() -> void:
+	toggle_pause()
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
