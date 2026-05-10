e66f8ebed67cd3368d279928a5a1e6659493943a
Bhavnoor Singh <KidNamedFinguh@protonmail.com>
2026-05-10 10:31:00 +1000

    Added the audio manager
    Use AudioManager.play_sfx("path/to/file", DB_adjustment) or AudioManager.play_music("path/to/file")

---
+++ scripts/audio_manager.gd
@@ -0,0 +1,29 @@
+extends Node
+
+var music_player: AudioStreamPlayer
+var sfx_bus = "SFX"
+var music_bus = "Music"
+
+func _ready():
+	music_player = AudioStreamPlayer.new()
+	add_child(music_player)
+	music_player.bus = music_bus
+	process_mode = Node.PROCESS_MODE_ALWAYS 
+
+func play_music(music_path: String):
+	var stream = load(music_path)
+	if music_player.stream == stream:
+		return
+	music_player.stream = stream
+	music_player.play()
+
+func play_sfx(sfx_path: String, volume_db: float = 0.0):
+	var stream = load(sfx_path)
+	var sfx_player = AudioStreamPlayer.new()
+	add_child(sfx_player)
+	sfx_player.pitch_scale = randf_range(0.9, 1.1)
+	sfx_player.stream = stream
+	sfx_player.bus = sfx_bus
+	sfx_player.volume_db = volume_db  # This controls the loudness
+	sfx_player.play()
+	sfx_player.finished.connect(sfx_player.queue_free)
